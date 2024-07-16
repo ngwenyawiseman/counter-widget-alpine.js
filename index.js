0f90a2555b0a46961bb644936728e4a0dea195c6
Thulebona Ngwenya <162568467+ngwenyawiseman@users.noreply.github.com>
2024-07-16 07:56:31 +0200

    new changes

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-function Counter() {
+function counter() {
     return {
         count: 0,
+        maxCount: 10,  // Set the max value for the counter
         increment() {
-            this.count++;
+            if (this.count < this.maxCount) {
+                this.count++;
+            }
         },
         decrement() {
             if (this.count > 0) {
                 this.count--;
             }
         },
+        reset() {
+            this.count = 0;
+        },
     };
 }
 
 // Ensure that AlpineJS is properly loaded
 document.addEventListener('alpine:init', () => {
-    Alpine.data('counter', Counter);
+    Alpine.data('counter', counter);
 });
